9294dfd0cd4a04e3964ef020d8231c853e40cbab
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-04-17 22:45:33 +0300

    Updates

---
+++ precache-manifest.3d3cb6307ae7a2c5fe148d9410d69432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37c730af53236cf57fc25075d79912c",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "31f6a8c23a3b39dc9fef",
+    "url": "/smart-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "95812035a69e149d9e9e",
+    "url": "/smart-brain/static/css/main.c902ce57.chunk.css"
+  },
+  {
+    "revision": "31f6a8c23a3b39dc9fef",
+    "url": "/smart-brain/static/js/2.c985a0da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.c985a0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95812035a69e149d9e9e",
+    "url": "/smart-brain/static/js/main.e5305279.chunk.js"
+  },
+  {
+    "revision": "3562ad85c6350642028f",
+    "url": "/smart-brain/static/js/runtime-main.8c759b17.js"
+  }
+]);
